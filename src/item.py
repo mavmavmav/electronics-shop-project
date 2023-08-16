ae857dca2ce159c20b2d959d25ae7f858148d841
maverino <maverino@vk.com>
2023-08-16 23:01:18 +0300

    code updated

--- src/item.py
+++ src/item.py
@@ -1,9 +1,14 @@
+import csv
+
+
 class Item:
+
     """
     Класс для представления товара в магазине.
     """
     pay_rate = 1.0
     all = []
+
 
     def __init__(self, name: str, price: float, quantity: int) -> None:
         """
@@ -13,7 +18,11 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        pass
+        self.name = name
+
+        self.price = price
+        self.quantity = quantity
+
 
     def calculate_total_price(self) -> float:
         """
@@ -21,10 +30,29 @@ class Item:
 
         :return: Общая стоимость товара.
         """
-        pass
+        return self.price * self.quantity
 
-    def apply_discount(self) -> None:
+
+    def apply_discount(self) -> float:
         """
         Применяет установленную скидку для конкретного товара.
         """
+        self.price = self.price * Item.pay_rate
+        return self.price
+
+    def instantiate_from_csv(self) -> list:
+        with open('C:/Users/007sh/Desktop/electronics-shop-project/src/items.csv', 'r') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                print(dict(row))
+
+
+
+
+
+    def string_to_number(self=str) -> int:
+
         pass
+
+item = Item('Телефон', 10000, 5)
+print(Item.instantiate_from_csv(self=item))
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,20 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from poetry.console.commands import self
+
+from src.item import Item
+
+item1 = Item("Смартфон", 10000, 20)
+item2 = Item("Ноутбук", 20000, 5)
+
+
+def test_calculate():
+    assert item1.calculate_total_price() == 200000
+    assert item2.calculate_total_price() == 100000
+
+
+def test_pay_rate():
+    assert Item.pay_rate == 0.8
+
+
+def test_discount():
+    assert item1.apply_discount() == 8000.0
