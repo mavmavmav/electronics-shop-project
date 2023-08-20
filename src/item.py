ac1df21dc5a6f38796f9cbc7b78dec0ede93a342
maverino <maverino@vk.com>
2023-08-20 23:30:52 +0300

    updated code

--- src/item.py
+++ src/item.py
@@ -36,7 +36,11 @@ class Item:
 
     @name.setter
     def name(self, name_string: str):
-        self.__name = name_string
+        if len(name_string) >= 10:
+            self.__name = name_string[:10] + '...'
+        else:
+            self.__name = name_string
+
 
     def calculate_total_price(self) -> float:
         """
