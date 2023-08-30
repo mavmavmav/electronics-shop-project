755f48f2ed76026e4da6da1f759c90358d5af8ed
maverino <maverino@vk.com>
2023-08-30 16:52:52 +0300

    created new subclass of Item class

---
+++ src/phone.py
@@ -0,0 +1,30 @@
+from item import Item
+
+
+class Phone(Item):
+    def __init__(self, name: str, price: float, quantity: int, number_of_sim):
+        super().__init__(name, price, quantity)
+        if number_of_sim > 0:
+            self.number_of_sim = number_of_sim
+        else:
+            raise ValueError('Количество сим-карт должно быть больше "0"')
+
+    def __add__(self, other):
+        """
+        Метод для операций сложения экземпляров класса
+        с проверкой отношений экземпляров
+        """
+        if isinstance(other, self.__class__):
+            return self.quantity + other.quantity
+        raise Exception
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+if __name__ == '__main__':
+    phone1 = Phone("iPhone 14", 120_000, 5, 2)
+    print(phone1.__dict__)
