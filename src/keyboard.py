e5912b1fdc64b8b9201d8d6e94bed78e4eb39ab2
maverino <maverino@vk.com>
2023-09-03 14:17:16 +0300

    new class Keyboard()

---
+++ src/keyboard.py
@@ -0,0 +1,22 @@
+from src.item import Item
+
+
+class Mixinlang:
+    def __init__(self):
+        self._language = 'EN'
+
+    @property
+    def language(self):
+        return self._language
+
+    def change_lang(self):
+        if self._language == 'EN':
+            self._language = 'RU'
+        else:
+            self._language = 'EN'
+        return self
+
+
+class Keyboard(Item, Mixinlang):
+    pass
+
